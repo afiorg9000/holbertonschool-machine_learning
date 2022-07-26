f7fac8268c99b93bfb558360bb9bcd8745ff5dd4
afiorg9000 <3533@holbertonschool.com>
2022-07-26 19:37:25 -0400

    2. Size Me Please

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+        
+    return shape
