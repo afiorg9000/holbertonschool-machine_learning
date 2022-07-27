0db4904e776f1994cf5feb7fa1fe93c8e997e725
afiorg9000 <3533@holbertonschool.com>
2022-07-27 19:47:27 -0400

    5. Across The Planes

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add_matrices2D(mat1, mat2):
+    added = []
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    for i in range (len(mat1)):
+        row = []
+        for j in range (len(mat2[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        added.append(row)
+    return added
