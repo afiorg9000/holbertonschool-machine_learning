4677d1c0367c6539dcd1d22c2bc12750b9451b9d
afiorg9000 <3533@holbertonschool.com>
2022-07-28 16:40:15 -0400

    comments

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""adds two matrices element-wise"""
+
+
 def add_matrices2D(mat1, mat2):
+    """return a new matrix"""
     added = []
     if len(mat1) != len(mat2):
         return None
