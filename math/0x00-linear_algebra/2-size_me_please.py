01ae2de3d2001dca2e38399adc5315c0d83ea5b8
afiorg9000 <3533@holbertonschool.com>
2022-07-28 16:35:14 -0400

    comments

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
+    """calculates the shape of a matrix"""
     shape = []
     while type(matrix) == list:
         shape.append(len(matrix))
