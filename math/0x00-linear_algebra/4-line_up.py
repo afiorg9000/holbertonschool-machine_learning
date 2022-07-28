8f7a2d9e4b33df2a2b7d8c49baf830970e2a8e0b
afiorg9000 <3533@holbertonschool.com>
2022-07-28 16:39:09 -0400

    comments

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""adds two arrays element-wise"""
+
 
 def add_arrays(arr1, arr2):
+    """return a new list"""
     added = []
     if len(arr1) != len(arr2):
         return None
