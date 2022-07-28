4c71ffe7e2ba8f19764d8ef266b261eedd7dd72b
afiorg9000 <3533@holbertonschool.com>
2022-07-28 16:06:19 -0400

    checkers

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """concatenates two matrices along a specific axis"""
-
-
 import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
     """concatenated"""
     return np.concatenate((mat1, mat2), axis)
