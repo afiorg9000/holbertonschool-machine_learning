1aff7c6a39fc09cbba156072e0dc56298ba06420
afiorg9000 <3533@holbertonschool.com>
2022-07-28 17:23:10 -0400

    Task 7 & 8 Completed

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """concatenates two matrices along a specific axis"""
+import numpy as np
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """return a new matrix"""
-    if len(mat1[0]) == len(mat2[0]):
-        return
+    if axis == 0:
+        new = [row.copy() for row in mat1] + mat2.copy()
+        return new
+    elif axis == 1:
+        new = [mat1[tmp] + mat2[tmp] for tmp in range(len(mat1))]
+        return new
+    else:
+        return None
--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""performs matrix multiplication"""
+import numpy as np
+
 
 def mat_mul(mat1, mat2):
-    if 
+    """performs matrix multiplication"""
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
+    if mat1.shape[1] != mat2.shape[0]:
+        return None
+    else:
+        return np.matmul(mat1, mat2).tolist()
