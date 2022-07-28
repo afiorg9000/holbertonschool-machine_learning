8ba7765cae6cb0be2a3fc451e91f41b23351d7eb
afiorg9000 <3533@holbertonschool.com>
2022-07-28 16:36:26 -0400

    comments

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""returns the transpose of a 2D matrix"""
+
+
 def matrix_transpose(matrix):
+    """ returns transpose"""
     transpose = []
     for i in range(len(matrix[0])):
         transpose.append([])
