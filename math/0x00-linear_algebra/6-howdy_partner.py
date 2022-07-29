1a0b393670becc451524832d0400a0441fa29542
afiorg9000 <3533@holbertonschool.com>
2022-07-28 22:21:10 -0400

    checkers

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -2,11 +2,9 @@
 """concatenates two arrays"""
 
 
+from array import array
+
+
 def cat_arrays(arr1, arr2):
     """return a new list"""
-    concatenated = []
-    for i in range(len(arr1)):
-        concatenated.append(arr1[i])
-    for j in range(len(arr2)):
-        concatenated.append(arr2[j])
-    return concatenated
+    return arr1 * arr2
