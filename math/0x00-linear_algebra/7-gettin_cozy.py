b6916adba7a2f04c97daf1d9d261307b2271f1c6
afiorg9000 <3533@holbertonschool.com>
2022-07-28 23:16:48 -0400

    check

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """concatenates two matrices along a specific axis"""
-import numpy as np
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """return a new matrix"""
-    if axis == 0:
-        return np.concatenate((mat1, mat2), axis=axis).tolist()
-    elif axis == 1:
-        return np.concatenate((mat1, mat2), axis=axis).tolist()
-    else:
-        return None
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        return mat1 + mat2
+    if axis == 1 and len(mat1) == len(mat2):
+        concatenated = []
+        for r1, r2 in zip(mat1, mat2):
+            concatenated.append(r1 + r2)
+        return concatenated
+    return None
