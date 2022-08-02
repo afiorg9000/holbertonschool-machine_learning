601cfd29aaa060fbc4f2f8952ce2947e662c484d
afiorg9000 <3533@holbertonschool.com>
2022-08-01 23:19:41 -0400

    plotting tasks complete

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.hist(student_grades, bins=range(0, 110, 10), edgecolor="black")
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()
