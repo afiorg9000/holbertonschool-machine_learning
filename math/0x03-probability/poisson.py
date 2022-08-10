83aff493a4a2cfbc0f8da544ee40a4195412d4ba
afiorg9000 <3533@holbertonschool.com>
2022-08-09 22:46:03 -0400

    1. Poisson PMF

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -9,7 +9,7 @@ class Poisson:
         if data is None:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
-            self.lambtha = float(lambtha)
+            self.lambtha = float(lambtha)  # value for average
         else:
             if type(data) is not list:
                 raise TypeError('data must be a list')
@@ -17,3 +17,20 @@ class Poisson:
                 raise ValueError('data must contain multiple values')
             else:
                 self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """probability mass function"""
+        e = 2.7182818285
+        if type(k) is not int:
+            k = int(k)  # number of occurences
+        if k < 0:
+            return 0
+        return ((self.
+                 lambtha ** k) * (e ** (- self.lambtha))) / self.factorial(k)
+
+    def factorial(self, k):
+        """factorial of k"""
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+        return factorial
