350823f2cfb303ab419b857d6463508c5a7b8e25
afiorg9000 <3533@holbertonschool.com>
2022-08-10 23:32:23 -0400

    Completed Normal Distribution

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -5,6 +5,9 @@
 class Normal:
     """normal distribution"""
 
+    pi = 3.1415926536
+    e = 2.7182818285
+
     def __init__(self, data=None, mean=0., stddev=1.):
         """normal distribution"""
         if data is None:
@@ -20,3 +23,23 @@ class Normal:
             self.mean = sum(data) / len(data)
             variance = [(x - self.mean) ** 2 for x in data]
             self.stddev = (sum(variance) / len(variance)) ** (1 / 2)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return float((x - self.mean)/self.stddev)
+
+    def x_value(self, z):
+        """"Calculates the x-value of a given z-score"""
+        return float(self.stddev*z + self.mean)
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        n = self.e**(-((x - self.mean)**2)/(2*self.stddev**2))
+        den = (self.stddev*(2*self.pi)**0.5)
+        return n / den
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given x-value"""
+        x = (x - self.mean)/(self.stddev*(2)**0.5)
+        erf = (2/(self.pi)**0.5)*(x - x**3/3 + x**5/10 - x**7/42 + x**9/216)
+        return 0.5*(1 + erf)
