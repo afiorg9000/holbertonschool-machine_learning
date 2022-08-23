19c5756746630d16c118905b972b31743515625f
afiorg9000 <3533@holbertonschool.com>
2022-08-23 10:59:31 -0400

    added the shape, size for the wights and biases

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -17,9 +17,9 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
 
-        self.W1 = np.random.normal(nodes, nx)
-        self.b1 = np.zeros(nodes, 1)
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros(shape=(nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.normal(1, nodes)
+        self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
         self.A2 = 0
