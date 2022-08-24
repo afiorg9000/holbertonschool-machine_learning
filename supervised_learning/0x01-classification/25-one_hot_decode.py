e96c979955b18fbdf09b0d18fa17620826e53ec4
afiorg9000 <3533@holbertonschool.com>
2022-08-23 21:31:07 -0400

    decode

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""converts a one-hot matrix into a vector of labels:"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a one-hot matrix into a vector of labels:"""
+    if (type(one_hot) == np.ndarray) and (len(one_hot.shape) is 2):
+        try:
+            return np.argmax(one_hot, axis=0)
+        except Exception:
+            return None
+    else:
+        return None
