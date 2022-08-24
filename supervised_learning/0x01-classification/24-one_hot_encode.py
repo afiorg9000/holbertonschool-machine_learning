f68c34966dd283febf4230d39cc26ab5bf8831d1
afiorg9000 <3533@holbertonschool.com>
2022-08-23 21:24:47 -0400

    rewrote the entire thing

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,10 +7,9 @@ def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix:"""
     m = Y.shape[0]
 
-    if len(Y.shape) != 1:
+    try:
+        Y_one_hot = np.zeros((classes, m))
+        Y_one_hot[Y, np.arange(m)] = 1
+        return Y_one_hot
+    except Exception:
         return None
-    out = np.zeros((classes, m))
-
-    for i in range(0, m):
-        out[Y[i]][i] = 1
-    return out
