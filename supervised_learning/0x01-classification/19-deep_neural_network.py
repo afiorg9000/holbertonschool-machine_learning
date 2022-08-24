62187781844881c09cf22ab736f843f696e92bac
afiorg9000 <3533@holbertonschool.com>
2022-08-23 20:30:00 -0400

    DeepNeuralNetworkw cost

---
+++ supervised_learning/0x01-classification/19-deep_neural_network.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """This code is initializing the deep neural network"""
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        for layer in range(self.L):
+            weight = "W" + str(layer + 1)
+            bias = "b" + str(layer + 1)
+            if layer == 0:
+                self.__weights[weight] = np.random.randn(layers[layer],
+                                                         nx) * np.sqrt(2 / nx)
+                self.__weights[bias] = np.zeros((layers[layer], 1))
+            else:
+                self.__weights[weight] = np.random.randn(
+                    layers[layer], layers[layer - 1]) * np.sqrt(
+                        2 / layers[layer - 1])
+                self.__weights[bias] = np.zeros((layers[layer], 1))
+
+    """getter for layer"""
+    @property
+    def L(self):
+        return(self.__L)
+
+    """getter for cache"""
+    @property
+    def cache(self):
+        return(self.__cache)
+
+    """getter for weight"""
+    @property
+    def weights(self):
+        return(self.__weights)
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        m = X.shape[1]
+        for lyr in range(self.__L + 1):
+            if lyr == 0:
+                self.__cache["A0"] = X
+            else:
+                Z = np.dot(self.__weights["W" + str(lyr)],
+                           self.__cache["A" + str(lyr - 1)]
+                           ) + self.__weights["b" + str(lyr)]
+                self.__cache["A" + str(lyr)] = 1 / (1 + np.exp(-Z))
+
+        return self.__cache["A" + str(self.__L)], self.__cache
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        C = -(1 / m)*np.sum(Y * np.log(A) + (1 - Y) * (np.log(1.0000001 - A)))
+        return C
