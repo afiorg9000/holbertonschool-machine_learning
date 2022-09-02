58307cbb901ececa9a1655852aeaaa72617b5c2d
afiorg9000 <3533@holbertonschool.com>
2022-09-02 18:18:03 -0400

    Batch Normalization

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -6,13 +6,11 @@ import numpy as np
 def batch_norm(Z, gamma, beta, epsilon):
     """normalizes an unactivated output using batch normalization:"""
     #  Compute the mean of the Zs in the layer
-    mean = Z.shape[0]
+    mean = np.mean(Z, axis=0)
     #  compute the variance of the Zs
     variance = np.mean((Z - mean)**2)
     #  Normalize the Z with mean and standard deviation
     Z_norm = (Z - mean) / (np.sqrt(variance + epsilon))
     #  Add some noise to the normalized Z so that they are reasonably different
-    Z_out = (gamma * Z_norm) + beta
-    #  Store parameters in cache
-    Cache = (Z, Z_norm, Z_out, mean, variance, gamma, beta)
-    return Z_out, Cache
+    return (gamma * Z_norm) + beta
+
