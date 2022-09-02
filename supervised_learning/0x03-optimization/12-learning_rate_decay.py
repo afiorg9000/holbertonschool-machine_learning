1d89296ceea947aff945df91369c250c263b0df5
afiorg9000 <3533@holbertonschool.com>
2022-09-02 17:51:14 -0400

    learning rate inverse

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""creates a learning rate decay operation"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """creates a learning rate decay operation"""
+    alpha = tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                        decay_rate, staircase=True)
+    return alpha
