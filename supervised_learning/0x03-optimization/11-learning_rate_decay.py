70ac2c91c1c971c676ffefdfd74658f66275439a
afiorg9000 <3533@holbertonschool.com>
2022-09-02 17:43:26 -0400

    learning rate

---
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""updates the learning rate using inverse time decay in numpy:"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """updates the learning rate using inverse time decay in numpy:"""
+    return alpha / (1 + decay_rate * (global_step // decay_step))
