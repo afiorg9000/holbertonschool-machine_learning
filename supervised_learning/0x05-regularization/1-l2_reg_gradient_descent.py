2cd3e89047c7056df7f1e251c932e3afecce0be8
afiorg9000 <3533@holbertonschool.com>
2022-09-07 12:36:31 -0400

    L2

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""updates the weights and biases of a neural network using gradient descent with L2 regularization:"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """updates the weights and biases of a neural network using gradient descent with L2 regularization:"""
+    for i in range(0, L):
+  first_derivative = -2 * np.dot(X.T, y - np.dot(X, weights)) + 2 * alpha * weights
+  weights = weights - (alpha / L) * first_derivative
+return weights
