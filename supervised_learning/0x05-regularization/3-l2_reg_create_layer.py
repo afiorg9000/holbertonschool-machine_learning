fd56c1e279918e8e88484fb1e9ff60dadaeeb39c
afiorg9000 <3533@holbertonschool.com>
2022-09-08 20:35:34 -0400

    3. Create a Layer with L2 Regularization

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -5,9 +5,11 @@ import tensorflow.compat.v1 as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creates a tensorflow layer that includes L2 regularization:"""
-    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode=("fan_avg"))
     kernel_regularizer = tf.keras.regularizers.L2(lambtha)
-    layer = tf.keras.layers.Dense(n, activation=activation,
-                                  kernel_initializer=initializer, name='layer',
-                                  kernel_regularizer=kernel_regularizer)
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=initializer,
+                            kernel_regularizer=kernel_regularizer,
+                            name='layer')
     return layer(prev)
