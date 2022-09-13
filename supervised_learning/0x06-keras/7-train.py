6a502b26e97461e661b57baeda96f1f015ac0278
afiorg9000 <3533@holbertonschool.com>
2022-09-13 13:32:22 -0400

    pycode

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -7,15 +7,18 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, learning_rate_decay=False,
                 alpha=0.1, decay_rate=1, verbose=True,
-                shuffle=False): 
-    """ model using mini-batch gradient descent:"""
+                shuffle=False):
+    """model using mini-batch gradient descent:"""
     stopping = []
     if validation_data:
         if early_stopping:
             stopping.append(K.callbacks.EarlyStopping(patience=patience))
         if learning_rate_decay:
             lr = 0
-            lr.append(K.callbacks.LearningRateScheduler(schedule=lambda epoch: alpha / (1 + decay_rate * epoch), verbose=1))
+            lr.append(K.callbacks.LearningRateScheduler(
+                          schedule=lambda epoch:
+                          alpha / (1 + decay_rate * epoch),
+                          verbose=1))
     iteration = network.fit(data, labels, batch_size=batch_size,
                             epochs=epochs,
                             verbose=verbose, shuffle=shuffle,
