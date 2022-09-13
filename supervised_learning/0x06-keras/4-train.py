d7175d08478f1b6608a89f39006f7d96b90f4a14
afiorg9000 <3533@holbertonschool.com>
2022-09-13 13:00:01 -0400

    does it work?

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""trains a model using mini-batch gradient descent:"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """ model using mini-batch gradient descent:"""
+    iteration = network.fit(data, labels, batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=verbose, shuffle=shuffle)
+    return iteration
