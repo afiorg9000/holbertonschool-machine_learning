1102b8a1ceec369aa02441f224417cd3c18be9a4
afiorg9000 <3533@holbertonschool.com>
2022-10-05 14:02:06 -0400

    transition layer fix

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -6,14 +6,13 @@ import tensorflow.keras as K
 def transition_layer(X, nb_filters, compression):
     """transition layer"""
     inititializer = K.initializers.he_normal()
-    activ1 = "relu"
     filt = int(nb_filters * compression)
 
     normal1 = K.layers.BatchNormalization()(X)
-    activ2 = K.layers.Activation(activ1)(normal1)
+    activ1 = K.layers.Activation('relu')(normal1)
     conv1 = K.layers.Conv2D(filters=filt, kernel_size=(1, 1),
-                             padding='same',
-                             kernel_initializer=inititializer)(activ1)
+                            padding='same',
+                            kernel_initializer=inititializer)(activ1)
     avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2),
                                          padding='same')(conv1)
     return avg_pool, filt
