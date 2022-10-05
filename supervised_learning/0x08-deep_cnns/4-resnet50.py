787fb4165abcc1819bec2ba93b1ede3182565fe0
afiorg9000 <3533@holbertonschool.com>
2022-10-05 13:49:26 -0400

    task 4

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ResNet-50 architecture"""
-from tkinter import W
 import tensorflow.keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
