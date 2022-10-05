6b81e6668e6a40dcf1ef946f898f9373cc8c950c
afiorg9000 <3533@holbertonschool.com>
2022-10-05 13:59:07 -0400

    deep block checker

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -5,23 +5,19 @@ import tensorflow.keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """dense block"""
-    initializer = K.initializers.he_normal()
-    activ1 = "relu"
+    initializer = K.initializers.HeNormal()
 
     for i in range(layers):
-        normal1 = K.layers.BatchNormalization()(X)
-        activ2 = K.layers.Activation(activ1)(normal1)
-        filt = 4 * growth_rate
-        bottleneck = K.layers.Conv2D(filters=filt, kernel_size=(1, 1),
-                                     padding='same',
-                                     kernel_initializer=initializer)(activ1)
-
-        normal2 = K.layers.BatchNormalization()(bottleneck)
-        activ2 = K.layers.Activation(activ1)(normal2)
-        conv = K.layers.Conv2D(filters=growth_rate, kernel_size=(3, 3),
-                               padding='same',
-                               kernel_initializer=initializer)(activ2)
-
-        X = K.layers.concatenate([X, conv])
+        batch_norm = K.layers.BatchNormalization()(X)
+        act = K.layers.Activation('relu')(batch_norm)
+        convol = K.layers.Conv2D(filters=4*growth_rate, kernel_size=(1, 1),
+                                 padding='same',
+                                 kernel_initializer=initializer)(act)
+        batch1 = K.layers.BatchNormalization()(convol)
+        act1 = K.layers.Activation('relu')(batch1)
+        convol1 = K.layers.Conv2D(filters=growth_rate, kernel_size=(3, 3),
+                                   padding='same',
+                                   kernel_initializer=initializer)(act1)
+        X = K.layers.concatenate([X, convol1])
         nb_filters += growth_rate
     return X, nb_filters
