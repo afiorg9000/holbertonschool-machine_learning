993d2d6fcbacd819003b303ee8fc5af35a951dfe
afiorg9000 <3533@holbertonschool.com>
2022-11-07 21:46:38 -0400

    mean and covarience

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -5,14 +5,16 @@ import numpy as np
 
 def mean_cov(X):
     """Calculates the mean and covariance of a data set."""
-
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
     # Calculate the mean: mean
     mean = np.mean(X, axis=0)
-
+    mean = mean.reshape(1, d)
     # Subtract the mean from X: centered_x
-    centered_x = X - mean
-
+    X_mean = X - mean
     # Calculate the covariance matrix using np.cov: cov
-    cov = np.cov(centered_x.T)
-
+    cov = np.matmul(X_mean.T, X_mean) / (n - 1)
     return (mean, cov)
