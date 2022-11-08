1936493adf3ff142fd241e5efedf5e2fbf02917d
afiorg9000 <3533@holbertonschool.com>
2022-11-07 22:51:41 -0400

    correlation

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -9,10 +9,9 @@ def correlation(C):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-
-    d = C.shape[0]  # Dimension of the covariance matrix
-
-    corr_matrix = (np.diag(1 / np.sqrt(np.diag(C)))).dot(
-        (np.linalg.inv(C)).dot((np.diag(1 / np.sqrt(np.diag(C))))))
-
-    return corr_matrix
+    d, _ = C.shape
+    cor = np.zeros((d, d))
+    for i in range(d):
+        for j in range(d):
+            cor[i][j] = C[i][j] / (np.sqrt(C[i][i] * C[j][j]))
+    return cor
