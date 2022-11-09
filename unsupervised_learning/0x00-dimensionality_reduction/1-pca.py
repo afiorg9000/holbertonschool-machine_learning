49f9a01e79af742fae19853350a843a560786a2f
afiorg9000 <3533@holbertonschool.com>
2022-11-09 13:02:44 -0400

    pcs v2

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,17 +1,19 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 """performs PCA on a dataset:"""
 import numpy as np
 
-def pca(X, var=0.95):
+
+def pca(X, ndim):
     """performs PCA on a dataset:"""
-    # Calculate the covariance matrix of X and find its eigenvalues and eigenvectors.
-    cov_matrix = np.cov(X, rowvar=False)  # (n x d) * (d x d)T = (n x n)
+    X_mean = np.mean(X, axis=0)
+    X_centered = X - X_mean
 
-    eigvals, eigvecs = np.linalg.eigh(cov_matrix)  # Eigenvalues are sorted in descending order by default
+    covar = np.dot(X_centered.T, X_centered) / len(X)
 
-    # Sort the components according to their explained variance: The first k columns of U represent the principal components in decreasing order of variance explained by each column of U . For example if you sort from largest to smallest explaination then the first column is most important for explaining 99% of the variance while the second column explains 87% and so on...
-    idx = np.argsort(-eigvals)  # Descending order sort indices based on eigenvalue magnitude
+    eigvals, eigvecs = np.linalg.eigh(covar)
 
-    W = eigvecs[:, idx]  # Get top k weights that maintain 95% variance explained by data points
+    idx = eigvals.argsort()[::-1]
 
-    return W
+    T = eigvecs[:, idx][:, :ndim]
+
+    return T
