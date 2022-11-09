8ef5fb7a14d5212cefec7b8e0e2d314cdf0a3b3b
afiorg9000 <3533@holbertonschool.com>
2022-11-09 13:04:06 -0400

    checker

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -6,15 +6,7 @@ import numpy as np
 def pca(X, var=0.95):
     """performs PCA on a dataset:"""
     cov_matrix = np.cov(X, rowvar=False)
-    # (n x d) * (d x d)T = (n x n)
-
     eigvals, eigvecs = np.linalg.eigh(cov_matrix)
-    # Eigenvalues are sorted in descending order by default
-
     idx = np.argsort(-eigvals)
-    # Descending order sort indices based on eigenvalue magnitude
-
     W = eigvecs[:, idx]
-    # Get top k weights that maintain 95% variance explained by data points
-
     return W
