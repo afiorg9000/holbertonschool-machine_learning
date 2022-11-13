419428a0c6241de2d8f6b51a5b2a625b44cb2500
afiorg9000 <3533@holbertonschool.com>
2022-11-13 18:10:09 -0400

    4. Continuous Posterior

---
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Calculates the posterior for bayes
+"""
+from scipy import special
+
+
+def posterior(x, n, p1, p2):
+    """calculates posterior probability"""
+    if (type(n) is not int) or (n <= 0):
+        raise ValueError("n must be a positive integer")
+
+    if (type(x) is not int) or (x < 0):
+        VE = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(VE)
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if type(p1) is not float or p1 < 0 or p1 > 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+
+    if type(p2) is not float or p2 < 0 or p2 > 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+
+    b2 = special.btdtr(x + 1, n - x + 1, p2)
+    b1 = special.btdtr(x + 1, n - x + 1, p1)
+    p = b2 - b1
+
+    return p
