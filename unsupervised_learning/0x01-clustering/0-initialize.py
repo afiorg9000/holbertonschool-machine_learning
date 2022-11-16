79da8a904ffc6f20dbfe13fab07fcaf4bd8f37e9
afiorg9000 <3533@holbertonschool.com>
2022-11-15 20:50:49 -0400

    pycode

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -2,10 +2,11 @@
 """initializes cluster centroids for K-means:"""
 import numpy as np
 
+
 def initialize(X, k):
     """initializes cluster centroids for K-means:"""
     d = X.shape[1]
-    min = np.amin(X,axis=0)
-    max = np.amax(X,axis=0)
-    centroides = np.random.uniform(low = min, high = max, size = (k,d))
+    min = np.amin(X, axis=0)
+    max = np.amax(X, axis=0)
+    centroides = np.random.uniform(low=min, high=max, size=(k, d))
     return centroides
