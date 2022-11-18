449ab9a45b608c915d02f8a00fe775f7d4acdbaa
afiorg9000 <3533@holbertonschool.com>
2022-11-17 20:42:40 -0400

    Maximization

---
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""maximization step in the EM algorithm for a GMM:"""
+import numpy as np
+
+
+def maximization(X, g):
+    """maximization step in the EM algorithm for a GMM:"""
+    n, d = X.shape
+    k, _ = g.shape
+    pr = np.sum(g, axis=0)
+    n_val = np.sum(pr)
+    if n_val != n:
+        return None, None, None
+    S = np.zeros((k, d, d))
+    pi = np.zeros((k,))
+    m = np.matmul(g, X) / np.sum(g, axis=1).reshape(-1, 1)
+    for i in range(k):
+        X_mean = X - m[i]
+        S[i] = np.matmul(np.multiply(g[i], X_mean.T), X_mean) / np.sum(g[i])
+        pi[i] = np.sum(g[i]) / n
+    return pi, m, S
