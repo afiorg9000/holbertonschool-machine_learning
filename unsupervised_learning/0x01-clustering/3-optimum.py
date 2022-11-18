e516fa65bc88dfb7691efc0b0389701f9f3b093a
afiorg9000 <3533@holbertonschool.com>
2022-11-17 20:09:10 -0400

    3. Optimize k

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-"""initializes cluster centroids for K-means:"""
+"""tests for the optimum number of clusters by variance:"""
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
-def initialize(X, k):
-    """initializes cluster centroids for K-means:"""
-    n = X.shape[0]  # Number of samples in dataset
 
-    randoms = np.random.randint(n, size=k)  # Randomly pick k indices out of n samples
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """tests for the optimum number of clusters by variance:"""
+    results = []
+    vars = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append(C)
+        results.append(clss)
+        vars.append(variance(X, C))
 
-    initialCentroids = X[randoms]  # Get corresponding sample points from dataset
+    d0 = vars[0]
+    d_list = []
+    for var in vars:
+        d_list.append(d0 - var)
+    return results, d_list
