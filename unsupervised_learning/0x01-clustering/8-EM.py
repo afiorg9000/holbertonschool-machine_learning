9645f5f3dcb9d81a888ed9a8bf7fc9e32a757196
afiorg9000 <3533@holbertonschool.com>
2022-11-17 20:51:41 -0400

    pycdo

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -5,6 +5,7 @@ initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
+
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """performs the expectation maximization for a GMM:"""
     pi, m, S = initialize(X, k)
