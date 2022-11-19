8c0f14f38675275d08cf4be40f2214c7bf16236a
afiorg9000 <3533@holbertonschool.com>
2022-11-18 21:20:34 -0400

    conditions

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -7,6 +7,18 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """tests for the optimum number of clusters by variance:"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if type(kmax) is not int or kmax < 1:
+        return None, None
+    if kmax <= kmin:
+        return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
     results = []
     vars = []
     for k in range(kmin, kmax + 1):
