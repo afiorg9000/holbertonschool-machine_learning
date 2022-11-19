5da57930c7945acb8e0c0afe92c461bcafcaeae4
afiorg9000 <3533@holbertonschool.com>
2022-11-18 21:51:40 -0400

    condition

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def maximization(X, g):
     """maximization step in the EM algorithm for a GMM:"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None, None
+    if type(g) is not np.ndarray or g.ndim != 2:
     n, d = X.shape
     k, _ = g.shape
     pr = np.sum(g, axis=0)
