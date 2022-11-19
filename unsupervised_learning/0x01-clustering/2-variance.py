8d6c38912eff563fecec035e1cdd78b655da6569
afiorg9000 <3533@holbertonschool.com>
2022-11-18 21:18:38 -0400

    check

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def variance(X, C):
     """calculates the total intra-cluster variance for a data set:"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    if type(C) is not np.ndarray or C.ndim != 2 or C.shape[1] != X.shape[1]:
+        return None
     N = np.expand_dims(X, axis=1)
     K = np.expand_dims(C, axis=0)
     dist = np.sum(np.square(N - K), axis=2)
