f5c594d358a3174df8fc9764c51c04b203786caf
afiorg9000 <3533@holbertonschool.com>
2022-11-18 21:40:22 -0400

    cchecke

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -6,15 +6,25 @@ pdf = __import__('5-pdf').pdf
 
 def expectation(X, pi, m, S):
     """calculates the expectation step in the EM algorithm for a GMM:"""
+        if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    d = X.shape[1]
+    if type(pi) is not np.ndarray or pi.ndim != 1 or not np.isclose(np.sum(pi), 1):
+        return None, None
+    k = pi.shape[0]
+    if type(m) is not np.ndarray or m.ndim != 2 or m.shape != (k, d):
+        return None, None
+    if type(S) is not np.ndarray or S.ndim != 3 or S.shape != (k, d, d):
+        return None, None
     n, d = X.shape
     k = pi.shape[0]
     if m.shape[0] != k or m.shape[1] != d:
         return None, None
     if S.shape[0] != k or S.shape[1] != d or S.shape[2] != d:
         return None, None
-    g = np.zeros((k, n))
+    gG = np.zeros((k, n))
     for i in range(k):
-        g[i] = pi[i] * pdf(X, m[i], S[i])
-    marginal = np.sum(g, axis=0)
+        gG[i] = pi[i] * pdf(X, m[i], S[i])
+    marginal = np.sum(gG, axis=0)
     l = np.sum(np.log(marginal))
-    return g / marginal, l
+    return gG / marginal, lL
