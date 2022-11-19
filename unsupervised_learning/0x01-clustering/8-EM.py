1921517e09eca283ae53ef006b206596eaddfa4e
afiorg9000 <3533@holbertonschool.com>
2022-11-18 21:54:03 -0400

    checker

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -8,6 +8,16 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """performs the expectation maximization for a GMM:"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None, None, None, None
+    if type(k) is not int or int(k) != k or k < 1:
+        return None, None, None, None, None
+    if type(iterations) is not int or int(iterations) != iterations or iterations < 1:
+        return None, None, None, None, None
+    if type(tol) is not float or tol < 0:
+        return None, None, None, None, None
+    if type(verbose) is not bool:
+        return None, None, None, None, None
     pi, m, S = initialize(X, k)
     l_old = 0
     for i in range(iterations):
