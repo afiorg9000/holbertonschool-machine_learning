6cfb152e0491862c5922127519ed039a8b08f3d3
afiorg9000 <3533@holbertonschool.com>
2022-11-29 13:32:45 -0400

    2. Update Gaussian Process

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Bayesian optimization on a noiseless 1D Gaussian process:"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+
+class BayesianOptimization:
+    """Bayesian optimization on a noiseless 1D Gaussian process:"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """class constructor"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        min, max = bounds
+        self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
