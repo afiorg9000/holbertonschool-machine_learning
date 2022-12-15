5662a93cda51fc2c756325e42296196c9b0b04ca
afiorg9000 <3533@holbertonschool.com>
2022-12-15 14:48:41 -0400

    check

---
+++ supervised_learning/0x0D-RNNs/7-bi_output.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""represents a bidirectional cell of an RNN:"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """represents a bidirectional cell of an RNN:"""
+    def __init__(self, i, h, o):
+        """represents a bidirectional cell of an RNN:"""
+        self.Whf = np.random.normal(size=(h + i, h))
+        self.Whb = np.random.normal(size=(h + i, h))
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """represents a bidirectional cell of an RNN:"""
+        h_next = np.tanh(np.matmul(np.hstack((h_prev, x_t)),
+                                   self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """backward"""
+        h_x = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.matmul(h_x, self.Whb) + self.bhb)
+        return h_prev
