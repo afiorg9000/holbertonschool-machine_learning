ce99143dc83f94f0b0b4f5f5e0d419e82ba01a64
afiorg9000 <3533@holbertonschool.com>
2022-12-20 01:02:49 -0400

    checker

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -11,5 +11,13 @@ def regular(P):
         return None
     if np.all(P * P) == 0:
         return None
-    A = np.linalg.matrix_power(P, 100)
-    return A[:, -1]
+    if P.shape[0] != P.shape[1]:
+        return None
+
+    n, _ = P.shape
+
+    eigen_val, eigen_vec = np.linalg.eig(P.T)
+    close_1 = np.isclose(eigen_val, 1)
+    target_v = eigen_vec[:, close_1]
+    target_v = target_v[:, 0]
+    return (target_v / np.sum(target_v)).reshape(1, n)
