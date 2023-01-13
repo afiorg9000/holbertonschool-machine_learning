2b38484ba0bad351462be9c12eb15baa23da4693
afiorg9000 <3533@holbertonschool.com>
2023-01-13 19:28:08 -0400

    check

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -12,7 +12,9 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
     if type(k) is not int or int(k) != k or k < 1:
         return None, None, None, None, None
-    if type(iterations) is not int or int(iterations) != iterations or iterations < 1:
+    if type(
+        iterations) is not int or int(
+            iterations) != iterations or iterations < 1:
         return None, None, None, None, None
     if type(tol) is not float or tol < 0:
         return None, None, None, None, None
@@ -21,14 +23,15 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     pi, m, S = initialize(X, k)
     l_old = 0
     for i in range(iterations):
-        g, l = expectation(X, pi, m, S)
+        g, lo = expectation(X, pi, m, S)
         if verbose is True and i % 10 == 0:
-            print("Log Likelihood after {} iterations: {}".format(i, l.round(5)))
-        if np.abs(l - l_old) <= tol:
+            print("Log Likelihood after {} iterations: {}".
+                  format(i, lo.round(5)))
+        if np.abs(lo - l_old) <= tol:
             break
         pi, m, S = maximization(X, g)
     g, lL = expectation(X, pi, m, S)
-    l_old =lL
+    l_old = lL
     if verbose is True:
-        print("Log Likelihood after {} iterations: {}".format(i, l.round(5)))
+        print("Log Likelihood after {} iterations: {}".format(i, lo.round(5)))
     return pi, m, S, g, lL
