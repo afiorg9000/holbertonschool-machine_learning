07048d0ce88ee4e889486538c394010c5192a9dd
afiorg9000 <3533@holbertonschool.com>
2023-01-20 17:40:08 -0400

    check

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -23,7 +23,7 @@ class Encoder(tf.keras.layers.Layer):
         seq_len = x.shape[1]
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += self.positional_encoding[:seq_len]
+        x += self.positional_encoding[None, :seq_len, :]
         x = self.dropout(x, training=training)
         for i in range(self.N):
             x = self.blocks[i](x, training, mask)
