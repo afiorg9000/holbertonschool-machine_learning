0c877db1209860378bbf4b8ee3af77fc44a759a7
afiorg9000 <3533@holbertonschool.com>
2023-01-22 17:09:49 -0400

    check

--- supervised_learning/0x11-attention/8-transformer_decoder_block.py
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -6,31 +6,34 @@ MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 class DecoderBlock(tf.keras.layers.Layer):
     """create an encoder block for a transformer:"""
+
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         """create an encoder block for a transformer:"""
+        super().__init__()
         self.mha1 = MultiHeadAttention(dm, h)
         self.mha2 = MultiHeadAttention(dm, h)
-        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
-        self.dense_output = tf.keras.layers.Dense(dm)
+        self.dense_hidden = tf.keras.layers.Dense(units=hidden,
+                                                  activation='relu')
+        self.dense_output = tf.keras.layers.Dense(units=dm)
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
         self.dropout3 = tf.keras.layers.Dropout(drop_rate)
-        super(DecoderBlock, self).__init__()
 
-    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
         """create an encoder block for a transformer:"""
-        start, weights1 = self.mha1(x, x, x, look_ahead_mask)
-        start = self.dropout1(start, training=training)
-        start = self.layernorm1(x + start)
-        mid, weights2 = self.mha2(start, encoder_output, encoder_output,
-                                  padding_mask)
-        mid = self.dropout2(mid, training=training)
-        mid = self.layernorm2(start + mid)
-        out = self.dense_hidden(mid)
-        out = self.dense_output(out)
-        out = self.dropout3(out, training=training)
-        out = self.layernorm3(mid + out)
-        return out
+        attention, attention_block = self.mha1(x, x, x, look_ahead_mask)
+        attention = self.dropout1(attention, training=training)
+        out1 = self.layernorm1(attention + x)
+        attention2, attn_weights_block2 = self.mha2(out1, encoder_output,
+                                                    encoder_output,
+                                                    padding_mask)
+        attention2 = self.dropout2(attention2, training=training)
+        out2 = self.layernorm2(attention2 + out1)
+        hidden_output = self.dense_hidden(out2)
+        output_output = self.dense_output(hidden_output)
+        ffn_output = self.dropout3(output_output, training=training)
+        output = self.layernorm3(ffn_output + out2)
+        return output
--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""create the encoder for a transformer:"""
 import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
@@ -8,23 +10,23 @@ class Encoder(tf.keras.layers.Layer):
 
     def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
                  drop_rate=0.1):
-        """create the encoder for a transformer:"""
-        super(Encoder, self).__init__()
+    """create the encoder for a transformer:"""
+        super().__init__()
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, self.dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
-                       for _ in range(N)]
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)
+                       ]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
         """create the encoder for a transformer:"""
         seq_len = x.shape[1]
-        x = self.embedding(x)
-        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += self.positional_encoding[None, :seq_len, :]
-        x = self.dropout(x, training=training)
+        embedding = self.embedding(x)
+        embedding *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        embedding += self.positional_encoding[:seq_len]
+        encoder_out = self.dropout(embedding, training=training)
         for i in range(self.N):
-            x = self.blocks[i](x, training, mask)
-        return x
+            encoder_out = self.blocks[i](encoder_out, training, mask)
+        return encoder_out
