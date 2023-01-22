a10da2164ceef58d7974c778165e142821dc1f80
afiorg9000 <3533@holbertonschool.com>
2023-01-22 17:14:32 -0400

    check

--- supervised_learning/0x11-attention/10-transformer_decoder.py
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -10,31 +10,24 @@ class Decoder(tf.keras.layers.Layer):
 
     def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len,
                  drop_rate=0.1):
-        """create the decoder for a transformer:"""
+    """create the decoder for a transformer:"""
+        super().__init__()
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
-        self.positional_encoding = self.positional_encoding(max_seq_len, dm)
-        self.blocks = [DecoderBlock(dm, h,
-                       hidden, drop_rate) for _ in range(N)]
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)
+                       ]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
-
-        super(Decoder, self).__init__()
 
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
         """create the decoder for a transformer:"""
-        seq_len = tf.shape(x)[1]
-        attention_weights = {}
-
+        seq_len = x.shape[1]
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
         x += self.positional_encoding[:seq_len]
         x = self.dropout(x, training=training)
-
         for i in range(self.N):
-            x, block1, block2 = self.blocks[i](x, encoder_output, training,
-                                               look_ahead_mask, padding_mask)
-            attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
-            attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
-
-        return x, attention_weights
+            x = self.blocks[i](x, encoder_output, training, look_ahead_mask,
+                               padding_mask)
+        return x
