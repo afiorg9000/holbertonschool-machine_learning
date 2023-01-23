15423ca631fac11d24959840a09994f0d96008fe
afiorg9000 <3533@holbertonschool.com>
2023-01-23 14:32:18 -0400

    ch

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -7,28 +7,28 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """tests for the optimum number of clusters by variance:"""
-    if type(X) is not np.ndarray or X.ndim != 2:
+    try:
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            return None, None
+        if not isinstance(iterations, int) or iterations < 1:
+            return None, None
+        if kmax is not None and (type(kmax) is not int or kmax < 1):
+            return None, None
+        if kmax is not None and kmin >= kmax:
+            return None, None
+        if kmax is None:
+            kmax = X.shape[0]
+        if not isinstance(kmin, int) or kmin < 1 or kmin >= X.shape[0]:
+            return None, None
+        results = []
+        d_vars = []
+        for k in range(kmin, kmax + 1):
+            cluster, clss = kmeans(X, k, iterations)
+            results.append((cluster, clss))
+            variance_d = variance(X, cluster)
+            if k == kmin:
+                variance_k = variance_d
+            d_vars.append(variance_k - variance_d)
+        return results, d_vars
+    except Exception:
         return None, None
-    if type(kmin) is not int or kmin < 1:
-        return None, None
-    if kmax is None:
-        kmax = X.shape[0]
-    if type(kmax) is not int or kmax < 1:
-        return None, None
-    if kmax <= kmin:
-        return None, None
-    if type(iterations) is not int or iterations < 1:
-        return None, None
-    results = []
-    vars = []
-    for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
-        results.append(C)
-        results.append(clss)
-        vars.append(variance(X, C))
-
-    d0 = vars[0]
-    d_vars = []
-    for var in vars:
-        d_vars.append(d0 - var)
-    return results, d_vars
