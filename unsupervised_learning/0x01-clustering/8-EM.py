605ae2262408a8b60caee7e2d77f5cb926617e56
afiorg9000 <3533@holbertonschool.com>
2023-01-23 14:41:43 -0400

    ch

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -8,30 +8,31 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """performs the expectation maximization for a GMM:"""
-    if type(X) is not np.ndarray or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if type(k) is not int or int(k) != k or k < 1:
+    if type(k) != int or k <= 0 or X.shape[0] < k:
         return None, None, None, None, None
-    if type(
-        iterations) is not int or int(
-            iterations) != iterations or iterations < 1:
+    if type(iterations) != int or iterations <= 0:
         return None, None, None, None, None
-    if type(tol) is not float or tol < 0:
+    if type(tol) != float or tol < 0:
         return None, None, None, None, None
-    if type(verbose) is not bool:
+    if type(verbose) != bool:
         return None, None, None, None, None
     pi, m, S = initialize(X, k)
-    l_old = 0
-    for i in range(iterations):
-        g, lo = expectation(X, pi, m, S)
-        if verbose is True and i % 10 == 0:
+    loglikelihood = 0
+    i = 0
+    while i < iterations:
+        g, loglikelihood_new = expectation(X, pi, m, S)
+        if verbose is True and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {}".
-                  format(i, lo.round(5)))
-        if np.abs(lo - l_old) <= tol:
+                  format(i, loglikelihood_new.round(5)))
+        if abs(loglikelihood_new - loglikelihood) <= tol:
             break
         pi, m, S = maximization(X, g)
-    g, lL = expectation(X, pi, m, S)
-    l_old = lL
+        i += 1
+        loglikelihood = loglikelihood_new
+    g, loglikelihood_new = expectation(X, pi, m, S)
     if verbose is True:
-        print("Log Likelihood after {} iterations: {}".format(i, lo.round(5)))
-    return pi, m, S, g, lL
+        print("Log Likelihood after {} iterations: {}".
+              format(i, loglikelihood_new.round(5)))
+    return pi, m, S, g, loglikelihood_new
