370f9df3b7d58d72f400c27aa9d324c7f8e1ede2
afiorg9000 <3533@holbertonschool.com>
2023-01-23 14:43:43 -0400

    jo

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -6,37 +6,39 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """clusters for a GMM using the Bayesian Information Criterion:"""
-    if type(X) is not np.ndarray or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
+    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
+        return None, None, None, None
+    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
+        return None, None, None, None
+    if kmin >= kmax:
+        return None, None, None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None, None, None
+    if type(tol) != float or tol <= 0:
+        return None, None, None, None
+    if type(verbose) != bool:
+        return None, None, None, None
+    k_best = []
+    best_res = []
+    logl_val = []
+    bic_val = []
     n, d = X.shape
-    if type(kmin) is not int or kmin != int(kmin) or kmin < 1:
-        return None, None, None, None
-    if kmax is None:
-        kmax = n
-    if type(kmax) is not int or kmax != int(kmax) or kmax < 1:
-        return None, None, None, None
-    if kmax <= kmin:
-        return None, None, None, None
-    if type(iterations) is not int or iterations != int(iterations):
-        return None, None, None, None
-    if type(tol) is not float or tol < 0:
-        return None, None, None, None
-    if type(verbose) is not bool:
-        return None, None, None, None
-
-    b = np.zeros(kmax + 1 - kmin)
-    lL = np.zeros(kmax + 1 - kmin)
-    results = []
     for k in range(kmin, kmax + 1):
-        pi, m, S, _, lL[k - kmin] = expectation_maximization(X,
-                                                             k,
-                                                             iterations=iterations,
-                                                             tol=tol, verbose=verbose)
-        results.append((pi, m, S))
-        p = k * (d + 2) * (d + 1) / 2 - 1
-        b[k - kmin] = p * np.log(n) - 2 * lL[k - kmin]
-
-    amin = np.argmin(b)
-    best_k = amin + kmin
-    best_result = results[amin]
-    return best_k, best_result, lL, b
+        pi, m, S, _, log_l = expectation_maximization(X, k, iterations, tol,
+                                                      verbose)
+        k_best.append(k)
+        best_res.append((pi, m, S))
+        logl_val.append(log_l)
+        cov_params = k * d * (d + 1) / 2.
+        mean_params = k * d
+        p = int(cov_params + mean_params + k - 1)
+        bic = p * np.log(n) - 2 * log_l
+        bic_val.append(bic)
+    bic_val = np.array(bic_val)
+    logl_val = np.array(logl_val)
+    best_val = np.argmin(bic_val)
+    k_best = k_best[best_val]
+    best_res = best_res[best_val]
+    return k_best, best_res, logl_val, bic_val
